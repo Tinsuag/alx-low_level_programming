72f5fbd7536a6df3b69ea219bcdd7b0a40fb8354
Tinsuag <tinsaeareg.work@gmail.com>
2023-06-26 22:47:37 +0300

    task 3 done

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -3,7 +3,7 @@
  * void swap_int(int *a, int *b) - used to swap two integers through the use of pointers
  * @a:number one integer
  * @b:number twi ubteger
- * @c:binterchanging variable
+ *
  * Return: always 0.
 */
 
---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,10 @@
+/***
+ * _strlen() -returns the size of a pointer
+ * @s: a pointer variable used as a measurement device
+ * 
+*/
+
+int _strlen(char *s)
+{
+    return sizeof(s);
+}
