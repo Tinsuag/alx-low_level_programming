79080c6f4af74f0de00fb3a4ef6361cda145c6d0
Tinsuag <tinsaeareg.work@gmail.com>
2023-06-26 22:25:13 +0300

    task 2

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * void swap_int(int *a, int *b) - used to swap two integers through the use of pointers
+ * @a:number one integer
+ * @b:number twi ubteger
+ * @c:binterchanging variable
+ * Return: always 0.
+*/
+
+void swap_int(int *a, int *b)
+{
+    int *c;
+    c=a;
+    *a=*b;
+    *b=c;
+}
