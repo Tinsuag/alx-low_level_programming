b0fa8ef5ae00f063d4f620bab14ac838b51be3f6
Tinsuag <tinsaeareg.work@gmail.com>
2023-06-26 21:11:43 +0300

    task 1

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,8 @@
+#include "main.h"
+#include <stdio.h>
+
+void reset_to_98(int *n){
+   int *p=*n;
+   p=98;
+
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+int n;
+
+    n = 402;
+    printf("n=%d\n", n);
+    reset_to_98(&n);
+    printf("n=%d\n", n);
+
+#endif /* MAIN_H */
