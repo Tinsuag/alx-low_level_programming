ae1d74508bd97959867944d81a0d3468e78f09e2
Tinsuag <tinsaeareg.work@gmail.com>
2023-06-26 21:58:57 +0300

    l

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,4 +1,13 @@
-void reset_to_98(int *n){
+/**
+ * reset_to_98 - takes a pointer to an int parameter
+ *                  and update the value to 98
+ * 
+ * @n: int parameter
+ * 
+ * Return: noting
+*/
+void reset_to_98(int *n)
+ {
    *n=98;
 
 }
